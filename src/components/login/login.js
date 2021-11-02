37c912053ba802e2bbe7484f6c998a9609900963
imanol309 <imanolguzman309@gmail.com>
2021-11-02 17:00:05 -0400

    first commit

---
+++ src/components/login/login.js
@@ -0,0 +1,11 @@
+import "./login.css";
+import logo from "../../assets/img/wave.svg";
+
+function Login() {
+  return (
+    <div className="container">
+      <img src={logo} alt="" className="imgFondo" />
+    </div>
+  );
+}
+export default Login;
