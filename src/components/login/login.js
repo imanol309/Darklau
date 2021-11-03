ec03a684741cdda861036165eccc9d977c9ad141
imanol309 <imanolguzman309@gmail.com>
2021-11-03 16:28:35 -0400

    Creacion del login de la pagina

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,11 +1,54 @@
 import "./login.css";
-import logo from "../../assets/img/wave.svg";
+import Comunicacion from '../../assets/img/comunicacion.jpg'
+import facebook from '../../assets/iconsRedes/facebbok.png'
+import google from '../../assets/iconsRedes/google.png'
+import twiter from '../../assets/iconsRedes/twiter.png'
+import wave from '../../assets/img/new_wave.png'
 
 function Login() {
   return (
     <div className="container">
-      <img src={logo} alt="" className="imgFondo" />
+      <img src={wave} alt="" className="imgFondo" />
+      <div className="containerLogin">
+        <div className="NavBarLogin">
+          <div className="TextPrincipalContainer">
+            <h3 className="TextPrincipal">Darklau</h3>
+          </div>
+          <div className="textNewUser">
+            <p className="textUser">New User?</p>
+            <p className="TextUp">Sign Up</p>
+          </div>
+        </div>
+        <div className="FormLogin">
+          <div className="FormImg">
+            <img src={Comunicacion} alt="" className="imgComunicacion"/>
+          </div>
+          <div className="FormInput">
+            <div className="FormTitulo">
+              <h1 className="textBack">Welcome Back!</h1>
+              <h5 className="textCon">Login to continue</h5>
+            </div>
+            <div className="containerForm">
+              <input type="text" className="inputGmail" placeholder="usename11@gmail.com"/>
+              <input type="password" className="inputPassword" placeholder="Enter Password" />
+            </div>
+            <div className="FormButton">
+              <button className="btnLogin">LOGIN</button>
+              <button className="btnPassword">FORGET PASSWORD?</button>
+            </div>
+            <div className="FormRedes">
+              <p className="tituloPrincipalRedes">Login With</p>
+              <div className="containerIconRedes">
+                <img src={google} alt="" className="imgRedes" />
+                <img src={facebook} alt="" className="imgRedes"/>
+                <img src={twiter} alt="" className="imgRedes" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default Login;
