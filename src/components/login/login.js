938cab32e66b3ca72df62eaa81117b9234e791c1
imanol309 <imanolguzman309@gmail.com>
2021-11-09 16:51:37 -0500

    Creacion el login completo

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,14 +1,12 @@
 import "./login.css";
-import Comunicacion from '../../assets/img/comunicacion.jpg'
-import facebook from '../../assets/iconsRedes/facebbok.png'
-import google from '../../assets/iconsRedes/google.png'
-import twiter from '../../assets/iconsRedes/twiter.png'
-import {useAuth0} from '@auth0/auth0-react'
+import Comunicacion from "../../assets/img/comunicacion.jpg";
+import facebook from "../../assets/iconsRedes/facebbok.png";
+import google from "../../assets/iconsRedes/google.png";
+import twiter from "../../assets/iconsRedes/twiter.png";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
-
-const {loginWithRedirect, logout } = useAuth0()
-
+  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
 
   return (
     <div className="container">
@@ -17,33 +15,48 @@ const {loginWithRedirect, logout } = useAuth0()
           <div className="TextPrincipalContainer">
             <h3 className="TextPrincipal">Darklau</h3>
           </div>
-          <div className="textNewUser">
-            <p className="textUser">New User?</p>
-            <p className="TextUp">Sign Up</p>
-          </div>
+          {
+            isAuthenticated ?
+            <div className="textNewUser">
+              <p className="TextUp" onClick={() => logout()}>LOGUOT</p>
+            </div>
+            :
+            null
+          }
         </div>
         <div className="FormLogin">
           <div className="FormImg">
-            <img src={Comunicacion} alt="" className="imgComunicacion"/>
+            <img src={Comunicacion} alt="" className="imgComunicacion" />
           </div>
           <div className="FormInput">
             <div className="FormTitulo">
-              <h1 className="textBack">Welcome Back!</h1>
+              <h1 className="textBack">
+                Welcome {isAuthenticated ? `${user.name}` : "Back!"}
+              </h1>
               <h5 className="textCon">Login to continue</h5>
             </div>
-            <div className="containerForm">
-              <input type="text" className="inputGmail" placeholder="usename11@gmail.com"/>
-              <input type="password" className="inputPassword" placeholder="Enter Password" />
-            </div>
             <div className="FormButton">
-              <button onClick={() => loginWithRedirect()} className="btnLogin">LOGIN</button>
-              <button onClick={() => logout()} className="btnPassword">FORGET PASSWORD?</button>
+              {isAuthenticated ? (
+                <div className="FormUsuario">
+                  <img src={user.picture} alt="" className="UsuarioImg" />
+                  <p className="UsuarioName">{user.name}</p>
+                  <p className="UsuarioEmail">{user.email}</p>
+                  <button className="btnUsuario">OPEN</button>
+                </div>
+              ) : (
+                <button
+                  onClick={() => loginWithRedirect()}
+                  className="btnLogin"
+                >
+                  LOGIN
+                </button>
+              )}
             </div>
             <div className="FormRedes">
-              <p className="tituloPrincipalRedes">Login With</p>
+              <p className="tituloPrincipalRedes">Redes Sociales</p>
               <div className="containerIconRedes">
                 <img src={google} alt="" className="imgRedes" />
-                <img src={facebook} alt="" className="imgRedes"/>
+                <img src={facebook} alt="" className="imgRedes" />
                 <img src={twiter} alt="" className="imgRedes" />
               </div>
             </div>
