0d69a7b3a7f414f58eaebabc3439c3ed0385e03a
imanol309 <imanolguzman309@gmail.com>
2022-01-30 23:53:44 -0400

    limpiando codigo para volverlo a retomar

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,14 +1,12 @@
 import "./login.css";
-import Comunicacion from '../../assets/img/comunicacion.jpg'
-import facebook from '../../assets/iconsRedes/facebbok.png'
-import google from '../../assets/iconsRedes/google.png'
-import twiter from '../../assets/iconsRedes/twiter.png'
-import {useAuth0} from '@auth0/auth0-react'
+import Comunicacion from "../../assets/img/comunicacion.jpg";
+import facebook from "../../assets/iconsRedes/facebbok.png";
+import google from "../../assets/iconsRedes/google.png";
+import twiter from "../../assets/iconsRedes/twiter.png";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
-
-const {loginWithRedirect, logout } = useAuth0()
-
+  const { loginWithRedirect, logout } = useAuth0();
 
   return (
     <div className="container">
@@ -24,7 +22,7 @@ const {loginWithRedirect, logout } = useAuth0()
         </div>
         <div className="FormLogin">
           <div className="FormImg">
-            <img src={Comunicacion} alt="" className="imgComunicacion"/>
+            <img src={Comunicacion} alt="" className="imgComunicacion" />
           </div>
           <div className="FormInput">
             <div className="FormTitulo">
@@ -32,18 +30,30 @@ const {loginWithRedirect, logout } = useAuth0()
               <h5 className="textCon">Login to continue</h5>
             </div>
             <div className="containerForm">
-              <input type="text" className="inputGmail" placeholder="usename11@gmail.com"/>
-              <input type="password" className="inputPassword" placeholder="Enter Password" />
+              <input
+                type="text"
+                className="inputGmail"
+                placeholder="usename11@gmail.com"
+              />
+              <input
+                type="password"
+                className="inputPassword"
+                placeholder="Enter Password"
+              />
             </div>
             <div className="FormButton">
-              <button onClick={() => loginWithRedirect()} className="btnLogin">LOGIN</button>
-              <button onClick={() => logout()} className="btnPassword">FORGET PASSWORD?</button>
+              <button onClick={() => loginWithRedirect()} className="btnLogin">
+                LOGIN
+              </button>
+              <button onClick={() => logout()} className="btnPassword">
+                FORGET PASSWORD?
+              </button>
             </div>
             <div className="FormRedes">
               <p className="tituloPrincipalRedes">Login With</p>
               <div className="containerIconRedes">
                 <img src={google} alt="" className="imgRedes" />
-                <img src={facebook} alt="" className="imgRedes"/>
+                <img src={facebook} alt="" className="imgRedes" />
                 <img src={twiter} alt="" className="imgRedes" />
               </div>
             </div>
